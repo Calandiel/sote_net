e6d894cfd2a9ffddf230e4a3f31faa23272e8765
Calandiel <Calandiel@users.noreply.github.com>
2021-05-27 16:50:56 +0200

    Added dummy functions to check if the DLL loads correctly in game

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[no_mangle]
+pub extern "C" fn initialize() -> bool {
+    true
+}
+
+#[no_mangle]
+pub extern "C" fn deinitialize() -> bool {
+	true
 }
