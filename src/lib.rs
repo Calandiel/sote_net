e9a7113b284c167fe7654f92823e2259ac5681f9
Calandiel <Calandiel@users.noreply.github.com>
2021-05-27 18:51:12 +0200

    Fixed a bug wherein boxes storing data were dropped too early

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,7 @@ pub extern "C" fn server_open(password: *const i8) -> bool {
 pub extern "C" fn crypto_get_secret() -> *const c_void {
 	let secret = StaticSecret::new(OsRng);
 	let boxed_secret = Box::new(secret);
-	return unsafe { Box::into_raw(boxed_secret) as *mut c_void };
+	return Box::into_raw(boxed_secret) as *mut c_void;
 }
 #[no_mangle]
 pub extern "C" fn crypto_get_public(secret_ptr: *mut c_void) -> *const c_void {
@@ -38,24 +38,28 @@ pub extern "C" fn crypto_get_public(secret_ptr: *mut c_void) -> *const c_void {
 	Box::into_raw(boxed_secret); // stop managing the memory
 
 	let boxed_public = Box::from(public);
-	return unsafe { Box::into_raw(boxed_public) as *mut c_void }
+	return Box::into_raw(boxed_public) as *mut c_void;
 }
 #[no_mangle]
 pub extern "C" fn crypto_get_shared(secret_ptr: *mut c_void, public_ptr: *mut c_void, payload: *mut u8) {
-	let boxed_secret = unsafe { Box::from_raw(secret_ptr as *mut StaticSecret) };
-	let boxed_public = unsafe { Box::from_raw(public_ptr as *mut PublicKey) };
+	let mut boxed_secret = unsafe { Box::from_raw(secret_ptr as *mut StaticSecret) };
+	let mut boxed_public = unsafe { Box::from_raw(public_ptr as *mut PublicKey) };
 
 	let secret = *boxed_secret;
 	let public = *boxed_public;
 
 	let shared = secret.diffie_hellman(&public);
 	let bytes = shared.as_bytes();
-	
 	unsafe {
 		for i in 0..32 {
 			*(payload.offset(i)) = bytes[i as usize];
 		}
 	}
+	*boxed_secret = secret;
+	*boxed_public = public;
+
+	Box::into_raw(boxed_secret);
+	Box::into_raw(boxed_public);
 }
 #[no_mangle]
 pub extern "C" fn crypto_drop_secret(secret_ptr: *mut c_void) { let _ = unsafe { Box::from_raw(secret_ptr as *mut StaticSecret) }; }
