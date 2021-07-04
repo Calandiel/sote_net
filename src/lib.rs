589aa1b9d2322d1f5545504fe6ec32d70a2713d8
Calandiel <Calandiel@users.noreply.github.com>
2021-07-04 18:15:04 +0200

    Fixed issues with the aes interface and added some simple rng for nonce generation

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 use rand_core::OsRng;
+use rand::prelude::*;
 use x25519_dalek::{StaticSecret, PublicKey};
 use std::ffi::c_void;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
-use sha3::{Digest, Sha3_256};
+use sha3::{Digest, Sha3_512};
+use aes_gcm_siv::{Aes256GcmSiv, Key, Nonce}; // Or `Aes128GcmSiv`
+use aes_gcm_siv::aead::{AeadInPlace, NewAead};
 
 
 // SOCKET
@@ -138,8 +141,8 @@ pub extern "C" fn crypto_get_shared(secret: *mut u8, public: *mut u8, payload: *
 // HASH
 #[no_mangle]
 pub extern "C" fn hash_sha3(sha_ptr : *mut *mut c_void) {
-	// create a SHA3-256 object
-	let hasher = Sha3_256::new();
+	// create a SHA3-512 object
+	let hasher = Sha3_512::new();
 	// box it
 	let b = Box::new(hasher);
 	// get a pointer
@@ -153,7 +156,7 @@ pub extern "C" fn hash_sha3(sha_ptr : *mut *mut c_void) {
 
 #[no_mangle]
 pub extern "C" fn hash_digest(sha_ptr : *mut c_void, input_buffer : *const u8, input_buffer_size : u32) {
-	let mut b = unsafe { Box::from_raw(sha_ptr as *mut Sha3_256) };
+	let mut b = unsafe { Box::from_raw(sha_ptr as *mut Sha3_512) };
 	let mut hasher = *b;
 
 	let buffer = unsafe { std::slice::from_raw_parts(input_buffer, input_buffer_size as usize) };
@@ -165,20 +168,110 @@ pub extern "C" fn hash_digest(sha_ptr : *mut c_void, input_buffer : *const u8, i
 
 #[no_mangle]
 pub extern "C" fn hash_finalize(sha_ptr : *mut c_void, output_buffer : *mut u8, output_buffer_size : u32) -> i32 {
-	if output_buffer_size != 32 {
+	if output_buffer_size != 64 {
 		return 1;
 	}
 	
-	let b = unsafe { Box::from_raw(sha_ptr as *mut Sha3_256) };
+	let b = unsafe { Box::from_raw(sha_ptr as *mut Sha3_512) };
 	let hasher = *b;
 
 	let result = hasher.finalize();
 
-	for elem in 0..32 {
+	for elem in 0..64 {
 		unsafe {
 			*(output_buffer.offset(elem)) = result[elem as usize];
 		}
 	}
 
 	return 0;
+}
+
+
+// AES
+#[no_mangle]
+pub extern "C" fn aes_encode(
+	message_ptr : *mut u8, message_size : u32,
+	key_ptr : *mut u8, key_size : u32,
+	nonce_ptr : *mut u8, nonce_size : u32,
+	output_ptr : *mut u8, output_size : u32,
+	encoded_output_size : *mut u32
+	) -> i32 {
+
+	let nonce_slice = unsafe { std::slice::from_raw_parts(nonce_ptr, nonce_size as usize) };
+	let nonce = Nonce::from_slice(nonce_slice);
+
+	let key_slice = unsafe { std::slice::from_raw_parts(key_ptr, key_size as usize) };
+	let key = Key::from_slice(key_slice);
+	let cipher = Aes256GcmSiv::new(key);
+
+	let message_slice = unsafe { std::slice::from_raw_parts(message_ptr, message_size as usize) };
+	let mut buffer = Vec::new();
+	buffer.extend_from_slice(message_slice);
+
+	// Encrypt `buffer` in-place, replacing the plaintext contents with ciphertext
+	match cipher.encrypt_in_place(nonce, b"", &mut buffer) {
+		Err(_) => return 1, // buffer was too short
+		_ => { }
+	};
+	if buffer.len() > output_size as usize {
+		return 2; // the output buffer provided to the function was too short
+	}
+	// Copy data to the output pointer
+	for elem in 0..buffer.len() {
+		unsafe {
+			*(output_ptr.offset(elem as isize)) = buffer[elem];
+		}
+	}
+	unsafe {
+		*encoded_output_size = buffer.len() as u32;
+	}
+	return 0;
+}
+
+#[no_mangle]
+pub extern "C" fn aes_decode(
+	message_ptr : *mut u8, message_size : u32,
+	key_ptr : *mut u8, key_size : u32,
+	nonce_ptr : *mut u8, nonce_size : u32,
+	output_ptr : *mut u8, output_size : u32,
+	decoded_output_size : *mut u32
+	) -> i32 {
+
+	let nonce_slice = unsafe { std::slice::from_raw_parts(nonce_ptr, nonce_size as usize) };
+	let nonce = Nonce::from_slice(nonce_slice);
+
+	let key_slice = unsafe { std::slice::from_raw_parts(key_ptr, key_size as usize) };
+	let key = Key::from_slice(key_slice);
+	let cipher = Aes256GcmSiv::new(key);
+
+	let message_slice = unsafe { std::slice::from_raw_parts(message_ptr, message_size as usize) };
+	let mut buffer = Vec::new();
+	buffer.extend_from_slice(message_slice);
+
+	// Encrypt `buffer` in-place, replacing the plaintext contents with ciphertext
+	match cipher.decrypt_in_place(nonce, b"", &mut buffer) {
+		Err(_) => return 1, // buffer was too short
+		_ => { }
+	};
+	if buffer.len() > output_size as usize {
+		return 2; // the output buffer provided to the function was too short
+	}
+	// Copy data to the output pointer
+	for elem in 0..buffer.len() {
+		unsafe {
+			*(output_ptr.offset(elem as isize)) = buffer[elem];
+		}
+	}
+	unsafe {
+		*decoded_output_size = buffer.len() as u32;
+	}
+	return 0;
+}
+
+
+// RNG
+#[no_mangle]
+pub extern "C" fn rand() -> u32 {
+	let mut rng = rand::thread_rng();
+	return rng.next_u32();
 }
